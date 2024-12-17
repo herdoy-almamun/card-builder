f2bbf691e49300c804dda441de25b16ba368e39a
= <=>
2024-12-17 13:17:52 +0530

    Build The Log In Page

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -11,7 +11,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { joiResolver } from "@hookform/resolvers/joi";
-import { Grid } from "@radix-ui/themes";
 import Joi from "joi";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -19,15 +18,11 @@ import { BeatLoader } from "react-spinners";
 import { toast } from "react-toastify";
 
 interface FormData {
-  firstName: string;
-  lastName: string;
   email: string;
   password: string;
 }
 
 const formSchema = Joi.object({
-  firstName: Joi.string().min(1).max(255).required().label("First Name"),
-  lastName: Joi.string().min(1).max(255).required().label("Last Name"),
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .max(1000)
@@ -36,13 +31,11 @@ const formSchema = Joi.object({
   password: Joi.string().min(8).max(1000).required().label("Password"),
 });
 
-const RegisterForm = () => {
+const LoginForm = () => {
   const [isLoading, setLoading] = useState(false);
   const form = useForm<FormData>({
     resolver: joiResolver(formSchema),
     defaultValues: {
-      firstName: "",
-      lastName: "",
       email: "",
       password: "",
     },
@@ -57,38 +50,8 @@ const RegisterForm = () => {
             toast.success("Form Submited"), setLoading(false);
           }, 1000);
         })}
-        className="space-y-3 w-full"
+        className="space-y-3"
       >
-        <Grid columns="1fr 1fr" gap="3">
-          <FormField
-            control={form.control}
-            name="firstName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>First Name</FormLabel>
-                <FormControl>
-                  <Input type="text" placeholder="First Name" {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="lastName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Last Name</FormLabel>
-                <FormControl>
-                  <Input type="text" placeholder="Last Name" {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </Grid>
         <FormField
           control={form.control}
           name="email"
@@ -118,11 +81,11 @@ const RegisterForm = () => {
           )}
         />
         <Button disabled={isLoading} className="w-full" type="submit">
-          {isLoading ? <BeatLoader /> : "Submit"}
+          {isLoading ? <BeatLoader /> : "Log In"}
         </Button>
       </form>
     </Form>
   );
 };
 
-export default RegisterForm;
+export default LoginForm;
